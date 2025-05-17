28d7c1c8f36abb16f0387aca2ff85b561ea67a1d
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-17 15:17:10 +0700

    membuat class mahasiswa

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -2,6 +2,18 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private:
+    int nim;
+    string nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
 int main(){
-    
+
 }
